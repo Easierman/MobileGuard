db9b528aaf0d1b327dc75d9fa2dfdccfa4a85667
Easierman <554925583@qq.com>
2017-10-22 21:06:33 +0800

    superman1

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/SetUp2Activity.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/SetUp2Activity.java
@@ -1,8 +1,14 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import cn.edu.gdmec.android.mobileguard.R;
 
@@ -10,17 +16,38 @@ import cn.edu.gdmec.android.mobileguard.R;
  * Created by Shinelon on 2017/9/21.
  */
 
-public class SetUp2Activity extends BaseSetUpAcitivity{
+public class SetUp2Activity extends BaseSetUpAcitivity implements View.OnClickListener{
+    private TelephonyManager mTelephonyManager;
+    private Button mBindSIMBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setup_2);
         ((RadioButton) findViewById(R.id.rb_second)).setChecked(true);
+        mTelephonyManager = (TelephonyManager) getSystemService(TELEPHONY_SERVICE);
+        mBindSIMBtn = (Button) findViewById(R.id.btn_bind_sim);
+        mBindSIMBtn.setOnClickListener(this);
+        if (isBind()){
+            mBindSIMBtn.setEnabled(false);
+        }else{
+            mBindSIMBtn.setEnabled(true);
+        }
+    }
 
+    private boolean isBind() {
+        String simString = sp.getString("sim", null);
+        if (TextUtils.isEmpty(simString)){
+            return false;
+        }
+        return true;
     }
 
     @Override
     public void showNext() {
+        if (!isBind()){
+            Toast.makeText(this,"您还没有绑定SIM卡！",Toast.LENGTH_LONG).show();
+            return;
+        }
         startActivityAndFinishSelf(SetUp3Activity.class);
     }
 
@@ -29,4 +56,26 @@ public class SetUp2Activity extends BaseSetUpAcitivity{
         startActivityAndFinishSelf(SetUp1Activity.class);
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_bind_sim:
+                bindSIM();
+                break;
+        }
+    }
+
+    private void bindSIM() {
+        if (!isBind()){
+            String simSerialNumber = mTelephonyManager.getSimSerialNumber();
+            SharedPreferences.Editor edit = sp.edit();
+            edit.putString("sim", simSerialNumber);
+            edit.commit();
+            Toast.makeText(this,"SIM卡绑定成功！",Toast.LENGTH_LONG).show();
+            mBindSIMBtn.setEnabled(false);
+        }else{
+            Toast.makeText(this,"SIM卡已经绑定！",Toast.LENGTH_LONG).show();
+            mBindSIMBtn.setEnabled(false);
+        }
+    }
 }
--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/adapter/ContactAdapter.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m2theftguard/adapter/ContactAdapter.java
@@ -1,8 +1,64 @@
 package cn.edu.gdmec.android.mobileguard.m2theftguard.adapter;
 
-/**
- * Created by Shinelon on 2017/9/21.
- */
+import android.content.Context;
 
-public class ContactAdapter {
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import android.widget.TextView;
+
+import java.util.List;
+
+import cn.edu.gdmec.android.mobileguard.R;
+
+import cn.edu.gdmec.android.mobileguard.m2theftguard.entity.ContactInfo;
+
+
+public class ContactAdapter extends BaseAdapter {
+    private List<ContactInfo> contactInfos;
+    private Context context;
+    public ContactAdapter(List<ContactInfo> contactInfos, Context context) {
+        super();
+        this.contactInfos=contactInfos;
+        this.context=context;
+    }
+
+    @Override
+    public int getCount() {
+        return contactInfos.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return contactInfos.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder holder=null;
+        if (convertView==null){
+            convertView = View.inflate(context, R.layout.item_list_contact_select,null);
+            holder = new ViewHolder();
+            holder.mNameTV=(TextView)convertView.findViewById(R.id.tv_name);
+            holder.mPhoneTv=(TextView)convertView.findViewById(R.id.tv_phone);
+            convertView.setTag(holder);
+        }else{
+            holder=(ViewHolder)convertView.getTag();
+        }
+        holder.mNameTV.setText(contactInfos.get(position).name);
+        holder.mPhoneTv.setText(contactInfos.get(position).phone);
+        return convertView;
+    }
+    static class ViewHolder{
+        TextView mNameTV;
+        TextView mPhoneTv;
+
+
+    }
 }
