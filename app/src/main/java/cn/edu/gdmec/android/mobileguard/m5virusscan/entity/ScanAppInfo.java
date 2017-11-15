2492c4a8f124170df83a753e18cfecd77a6157db
Easierman <554925583@qq.com>
2017-11-15 21:18:04 +0800

    2017.11.13 第五章手机杀毒 10.6 已经敲完

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/entity/ScanAppInfo.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m5virusscan/entity/ScanAppInfo.java
@@ -1,8 +1,15 @@
 package cn.edu.gdmec.android.mobileguard.m5virusscan.entity;
 
+import android.graphics.drawable.Drawable;
+
 /**
  * Created by Shinelon on 2017/9/21.
  */
 
 public class ScanAppInfo {
+    public String appName;
+    public boolean isVirus;
+    public String packagename;
+    public String description;
+    public Drawable appicon;
 }
