dac365852abb2bb1c28c6724e324162650263401
Easierman <554925583@qq.com>
2017-11-21 22:41:38 +0800

    11.20 10.14 CacheClearListActivity

--- app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/entity/CacheInfo.java
+++ app/src/main/java/cn/edu/gdmec/android/mobileguard/m6cleancache/entity/CacheInfo.java
@@ -1,8 +1,14 @@
 package cn.edu.gdmec.android.mobileguard.m6cleancache.entity;
 
+import android.graphics.drawable.Drawable;
+
 /**
  * Created by Shinelon on 2017/9/21.
  */
 
 public class CacheInfo {
+    public String packagename;
+    public long cacheSize;
+    public Drawable appIcon;
+    public String appName;
 }
